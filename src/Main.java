94cad86e9542661c523461093b4cad3b32f74c98
Neposeda644 <denis_bulyga@mail.ru>
2020-09-29 18:03:08 +0300

    Initial comit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 1280;
+        int mile = 20;
+
+	    long bonus = price / mile;
+        System.out.println(bonus) ;
+    }
+}
